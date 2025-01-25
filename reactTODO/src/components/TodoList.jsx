f4e82792b43e8ed353ed55a51ed38936d4bb3abd
Pradesh Gwachha <pradesh248@gmail.com>
2025-01-25 13:50:25 -0600

    todo app

---
+++ reactTODO/src/components/TodoList.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import Form from "./Form";
+import { v4 as uuidv4 } from "uuid";
+import Todo from "./Todo";
+import Edit from "./Edit";
+uuidv4();
+
+const TodoList = () => {
+  const [todoValue, setTodo] = useState([]);
+  const createTodo = (todo) => {
+    setTodo([...todoValue, { id: uuidv4(), task: todo, isEditing: false }]);
+  };
+
+  const deleteTodo = (id) => {
+    setTodo(todoValue.filter((todo) => todo.id !== id));
+  };
+
+  const editTodo = (id) => {
+    setTodo(
+      todoValue.map((todo) =>
+        todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
+
+  const editTask = (task, id) => {
+    setTodo(
+      todoValue.map((todo) =>
+        todo.id === id ? { ...todo, task, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
+
+  return (
+    <div className="container bg-gray-700 mt-20 p-8 rounded-md w-[550px] ">
+      <div className="text-white mb-5 text-3xl flex justify-center items-center">
+        Todo App
+      </div>
+      <Form createTodo={createTodo} />
+      <div className="text-white mb-2 text-xl">Todo tasks</div>
+      {todoValue.map((todo, idx) =>
+        todo.isEditing ? (
+          <Edit key={idx} editTodo={editTask} task={todo} />
+        ) : (
+          <Todo
+            task={todo}
+            key={idx}
+            deleteTodo={deleteTodo}
+            editTodo={editTodo}
+          />
+        )
+      )}
+    </div>
+  );
+};
+
+export default TodoList;
